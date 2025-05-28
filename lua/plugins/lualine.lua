5b9abd6e7ec88feee5175498480dd063d80cd971
ialobanov <ivan.a.lobanov@gmail.com>
2025-05-28 14:43:37 +0300

    Work with repository

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
-      { 'echasnovski/mini.nvim', version = '*' },
+      --{ 'echasnovski/mini.nvim', version = false },
     },
     opts = function()
       return {
